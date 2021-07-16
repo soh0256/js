c332abbf1c7984ff570dd3a362503b1e4098a842
Park jiseon <soh0256@naver.com>
2021-07-16 10:23:58 +0900

    commit

--- src/main/java/com/react/spring/board/BoardController.java
+++ src/main/java/com/react/spring/board/BoardController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+// STS_GIT TEST
 @Controller
 public class BoardController {
 
